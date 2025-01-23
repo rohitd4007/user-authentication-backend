e567479fb5ca14be400b2e6887b96c79bfd45282
Rohit Devhare <rohitdevhare400@gmail.com>
2025-01-23 16:02:18 +0530

    added location

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
+const geoip = require('geoip-lite');
 
 dotenv.config();
 
@@ -23,6 +24,27 @@ app.use(cors(corsOptions));
 
 // Connect to the database
 connectDB();
+
+
+// Trust proxy to get correct IP
+app.set('trust proxy', true);
+
+// Define a route to get location from IP
+app.get('/api/location-from-ip', (req, res) => {
+    const forwarded = req.headers['x-forwarded-for'];
+    const ip = forwarded ? forwarded.split(',')[0] : req.ip; // Get client IP
+    console.log("IP::", ip)
+    const geo = geoip.lookup(ip);
+    console.log("Geo::", geo)
+
+
+    if (geo) {
+        const { city, region, country, ll } = geo; // ll = [latitude, longitude]
+        res.status(200).json({ city, region, country, latitude: ll[0], longitude: ll[1] });
+    } else {
+        res.status(404).json({ message: 'Location not found' });
+    }
+});
 
 // Routes
 app.use('/api/auth', authRoutes);
