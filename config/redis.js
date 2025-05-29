a48a7192b8d1311a0e81a239ab8dc6d3f4c2c4a0
Rohit Devhare <rohitdevhare400@gmail.com>
2025-05-29 17:58:20 +0530

    redis issue fixed

--- config/redis.js
+++ config/redis.js
@@ -1,9 +1,9 @@
 const Redis = require("ioredis");
 
 const redis = new Redis({
-    host: "redis-16801.c277.us-east-1-3.ec2.redns.redis-cloud.com",
-    port: 16801,
-    password: "fd6SKXS9UXnXQYZHDkdCEIQga9mlIHyX",
+    host: "redis-12845.c89.us-east-1-3.ec2.redns.redis-cloud.com",
+    port: 12845,
+    password: "qIcTTo2AmoddbNQCUtdinWE7wv8a6VCj",
 });
 
 redis.on("connect", () => console.log("Connected to Redis Cloud"));
